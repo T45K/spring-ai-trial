03af6446bf43b24d96cb04d8dd9ad3f17adb2d3a
T45K <tasktas9@gmail.com>
2024-08-10 23:23:29 +0900

    add sample

--- src/main/kotlin/t45k/github/io/springAiTrial/SpringAiTrialApplication.kt
+++ src/main/kotlin/t45k/github/io/springAiTrial/SpringAiTrialApplication.kt
@@ -1,11 +1,23 @@
 package t45k.github.io.springAiTrial
 
+import org.springframework.ai.embedding.EmbeddingModel
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SpringAiTrialApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringAiTrialApplication>(*args)
+    runApplication<SpringAiTrialApplication>(*args)
+}
+
+@RestController
+class Controller(private val embeddingModel: EmbeddingModel) {
+    @GetMapping("/ai")
+    fun call() {
+        val response = embeddingModel.embedForResponse(listOf("hello"))
+        println(response.results.map { it.output })
+    }
 }

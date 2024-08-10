55f17d0a57f8502a6297a90ca2c482c779af9dcc
T45K <tasktas9@gmail.com>
2024-08-10 23:42:30 +0900

    add impl

--- src/main/kotlin/t45k/github/io/springAiTrial/SpringAiTrialApplication.kt
+++ src/main/kotlin/t45k/github/io/springAiTrial/SpringAiTrialApplication.kt
@@ -1,5 +1,6 @@
 package t45k.github.io.springAiTrial
 
+import kotlin.math.sqrt
 import org.springframework.ai.embedding.EmbeddingModel
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -16,8 +17,53 @@ fun main(args: Array<String>) {
 @RestController
 class Controller(private val embeddingModel: EmbeddingModel) {
     @GetMapping("/ai")
-    fun call() {
-        val response = embeddingModel.embedForResponse(listOf("hello"))
-        println(response.results.map { it.output })
+    fun call(): Any {
+        val response = embeddingModel.embedForResponse(
+            listOf(
+                sumMethod1.trimIndent(),
+                sumMethod2.trimIndent(),
+                sumMethod3.trimIndent(),
+                productMethod.trimIndent(),
+            )
+        )
+
+        val vectors = response.results.map { it.output }
+        return vectors.map { vector -> vectors.map { calcCosSimilarity(vector, it) } }
     }
 }
+
+private fun calcCosSimilarity(vector1: List<Double>, vector2: List<Double>): Double {
+    val a = vector1.zip(vector2) { a, b -> a * b }.sum()
+    val b = sqrt(vector1.sumOf { it * it }) * sqrt(vector2.sumOf { it * it })
+    return a / b
+}
+
+private const val sumMethod1 = """
+    fun sum(values: List<Int>): Int {
+        var sum = 0
+        for (value in values) {
+            sum += value
+        }
+        return sum
+    }
+"""
+
+private const val sumMethod2 = """
+    fun sum(values: List<Int>): Int {
+        return values.reduce { acc, value -> acc + value }
+    }
+"""
+
+private const val sumMethod3 = """
+    fun sum(values: List<Int>): Int = values.sum()
+"""
+
+private const val productMethod = """
+    fun product(values: List<Int>): Int {
+        var product = 1
+        for (value in values) {
+            product *= value
+        }
+        return product
+    }
+"""
